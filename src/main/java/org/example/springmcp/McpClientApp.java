b2c678cdd5fb562dc45e2f1fe3d3b46a370b5312
Berat Ölmez <159007239+seberatolmez@users.noreply.github.com>
2025-12-17 22:11:13 +0300

    feat: implement McpClient that connects to the server

---
+++ src/main/java/org/example/springmcp/McpClientApp.java
@@ -0,0 +1,29 @@
+package org.example.springmcp;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.modelcontextprotocol.client.McpSyncClient;
+import io.modelcontextprotocol.client.transport.ServerParameters;
+import io.modelcontextprotocol.client.transport.StdioClientTransport;
+import io.modelcontextprotocol.json.jackson.JacksonMcpJsonMapper;
+import io.modelcontextprotocol.spec.McpClientTransport;
+
+public class McpClientApp {
+
+    public static McpSyncClient getClient() {
+        ServerParameters params = ServerParameters
+                .builder("npx")
+                .args("-y", "@modelcontextprotocol/server-everything")
+                .build();
+
+        JacksonMcpJsonMapper jsonMapper = new JacksonMcpJsonMapper(new ObjectMapper());
+        McpClientTransport transport = new StdioClientTransport(params, jsonMapper);
+
+        return io.modelcontextprotocol.client.McpClient.sync(transport)
+                .build();
+    }
+
+    public static void main(String[] args) {
+        McpSyncClient client = getClient();
+        client.initialize();
+    }
+}
