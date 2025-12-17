0ad7fc8bfc7e2e5d2494e866221a86cf035a107f
Berat Ölmez <159007239+seberatolmez@users.noreply.github.com>
2025-12-17 21:46:37 +0300

    feat: implement McpServer to tell clients which protocol operations are avaliable

---
+++ src/main/java/org/example/springmcp/McpServerApp.java
@@ -0,0 +1,29 @@
+package org.example.springmcp;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.modelcontextprotocol.json.jackson.JacksonMcpJsonMapper;
+import io.modelcontextprotocol.server.McpServer;
+import io.modelcontextprotocol.server.McpSyncServer;
+import io.modelcontextprotocol.server.transport.StdioServerTransportProvider;
+import io.modelcontextprotocol.spec.McpSchema;
+
+public class McpServerApp {
+
+    public static McpSyncServer createServer() {
+        JacksonMcpJsonMapper jsonMapper = new JacksonMcpJsonMapper(new ObjectMapper());
+        StdioServerTransportProvider transportProvider = new StdioServerTransportProvider(jsonMapper);
+
+        return McpServer.sync(transportProvider)
+                .serverInfo("spring-mcp-demo-server","0.0.1")
+                .capabilities(McpSchema.ServerCapabilities.builder()
+                        .tools(true)
+                        .logging()
+                        .build())
+                .tools(LoggingTool.logPromptTool())
+                .build();
+    }
+
+    public static void main(String[] args) {
+        createServer();
+    }
+}
